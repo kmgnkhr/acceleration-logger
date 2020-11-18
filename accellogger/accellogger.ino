60aabc3cb9df585a09a5885ad7efb3d738384543
kmgnkhr <49336378+kmgnkhr@users.noreply.github.com>
2020-11-18 21:02:07 +0900

    Update accellogger.ino
    Parse command arguments.
    Change to command finalization by typing a newline.

--- accellogger/accellogger.ino
+++ accellogger/accellogger.ino
@@ -12,26 +12,49 @@ IMU6886 imu;
 BluetoothSerial serialBT;
 Logger logger;
 
+int read_argument(Stream* stream) {
+  String str = "";
+  while (true) {
+    while (!stream->available()) {}
+    auto c = stream->read();
+    stream->print(static_cast<char>(c));
+    if (c == 0xa || c == 0xd) break;
+    str += static_cast<char>(c);
+  }
+  auto i = str.toInt();
+  return i > 0 ? i : 1000;
+}
+
+void start(Stream* stream) {
+  auto i = read_argument(stream);
+  stream->print("begin (");
+  stream->print(i);
+  stream->println("us)");
+  logger.Start(&imu, i, stream);
+  stream->print("end (");
+  stream->print(logger.LoggedPeriod());
+  stream->println("us)");
+}
+
 void command(Stream* stream) {
   if (stream == nullptr || stream->available() == 0) return;
 
   const auto c = stream->read();
+  stream->print(static_cast<char>(c));
   switch (::tolower(c)) {
     case 'c':
+      read_argument(stream);
       ::digitalWrite(10, LOW);
       imu.calibrate();
       ::digitalWrite(10, HIGH);
       break;
     case 'b':
       ::digitalWrite(10, LOW);
-      stream->println("begin");
-      logger.Start(&imu, 1000, stream);
-      stream->print("end (");
-      stream->print(logger.LoggedPeriod());
-      stream->println("us)");
+      start(stream);
       ::digitalWrite(10, HIGH);
       break;
     case 's':
+      read_argument(stream);
       logger.Flush(stream);
       break;
     default:
