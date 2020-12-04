7fe6b919d284800d96f428485b66628ef8c88e59
kmgnkhr <49336378+kmgnkhr@users.noreply.github.com>
2020-12-04 21:54:02 +0900

    Update logger.cpp
    Extend the number of significant digits.

--- accellogger/logger.cpp
+++ accellogger/logger.cpp
@@ -33,9 +33,9 @@ void Logger::Start(IMU6886* imu, uint32_t interval_us, Stream* stream) {
     float x, y, z;
     imu->get(&x, &y, &z);
     log.micros = now - start;
-    log.x = static_cast<int16_t>(x * 100.F);
-    log.y = static_cast<int16_t>(y * 100.F);
-    log.z = static_cast<int16_t>(z * 100.F);
+    log.x = static_cast<int16_t>(x * 10000.F);
+    log.y = static_cast<int16_t>(y * 10000.F);
+    log.z = static_cast<int16_t>(z * 10000.F);
 
     last = now;
     ++index_;
@@ -46,9 +46,9 @@ void Logger::Flush(Stream* stream) {
   for (auto i = 0; i < index_; ++i) {
     const auto& log = buffer_[i];
     auto line = String(log.micros) + "," +
-                String(log.x / 100.F) + "," +
-                String(log.y / 100.F) + "," +
-                String(log.z / 100.F);
+                String(log.x / 10000.F, 4) + "," +
+                String(log.y / 10000.F, 4) + "," +
+                String(log.z / 10000.F, 4);
     stream->println(line);
   }
   index_ = 0;
